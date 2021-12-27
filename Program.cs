74e2e71c5ca058799ccdce9c6569629716f9ba89
danthespal <55256796+danthespal@users.noreply.github.com>
2021-12-28 01:39:46 +0200

    ram optimisation
    after some tests with LINQ I found that it's using to much ram, so I used a Dictionary for optimisation.

--- Program.cs
+++ Program.cs
@@ -1,39 +1,66 @@
-﻿public class RecursiveFileProcessor
+﻿namespace ExtGet
 {
-    public static void Main(string[] args)
+    public class RecursiveFileProcessor
     {
-        if(args.Length == 0)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("\nExtGet v0.1 BETA\n");
-            Console.WriteLine("you should use the program with a path argument.\n");
-            Console.WriteLine("eg: ExtGet.exe <path>\n");
-        }
-        else
-        {
-            foreach (string path in args)
+            if (args.Length == 0)
             {
-                if (File.Exists(path) || Directory.Exists(path))
+                Console.WriteLine("\nExtGet v0.1 BETA\n");
+                Console.WriteLine("you should use the program with a path argument.\n");
+                Console.WriteLine("eg: ExtGet.exe <path>\n");
+            }
+            else
+            {
+                foreach (string path in args)
                 {
-                    // this path is a file or directory
-                    ProcessFiles(path);
-                }
-                else
-                {
-                    Console.WriteLine($"{path} is not a valid file or directory.");
+                    if (File.Exists(path) || Directory.Exists(path))
+                    {
+                        // this path is a file or directory
+                        ProcessFiles(path);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{path} is not a valid file or directory.");
+                    }
                 }
             }
         }
-    }
 
-    public static void ProcessFiles(string targetDirectory)
-    {
-        var files = Directory.GetFiles(targetDirectory, "*.*", SearchOption.AllDirectories).GroupBy(p => Path.GetExtension(p));
-
-        Console.WriteLine("\nExtGet v0.1 BETA\n");
-
-        foreach (var file in files)
+        public static void ProcessFiles(string targetDirectory)
         {
-            Console.WriteLine(file.Key + " : " + file.Count());
+            Dictionary<string, int> extensions = new();
+
+            void CalcFilesCount(string targetDirectory)
+            {
+                IEnumerable<IGrouping<string, string>>? files = Directory.GetFiles(targetDirectory).GroupBy(p => Path.GetExtension(p));
+
+                foreach (IGrouping<string, string>? ex in files)
+                {
+                    if (extensions.ContainsKey(ex.Key))
+                    {
+                        extensions[ex.Key] += ex.Count();
+                    }
+                    else
+                    {
+                        extensions[ex.Key] = ex.Count();
+                    }
+                }
+
+                foreach (string? p2 in Directory.GetDirectories(targetDirectory))
+                {
+                    CalcFilesCount(p2);
+                }
+            }
+
+            CalcFilesCount(targetDirectory);
+
+            Console.WriteLine("\nExtGet v0.1 BETA\n");
+
+            foreach (KeyValuePair<string, int> file in extensions)
+            {
+                Console.WriteLine(file.Key + " : " + file.Value);
+            }
         }
     }
 }
