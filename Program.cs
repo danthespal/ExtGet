8013f52cf04bd0141ee419d532541c1138913ffd
danthespal <55256796+danthespal@users.noreply.github.com>
2021-12-28 00:46:26 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public class RecursiveFileProcessor
+{
+    public static void Main(string[] args)
+    {
+        string path = args[0];
+        var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).GroupBy(p => Path.GetExtension(p));
+
+        Console.WriteLine("\nExtGet v0.1 BETA\n");
+
+        foreach (var file in files)
+        {
+            Console.WriteLine(file.Key + " : " + file.Count());
+        }
+    }
+}
