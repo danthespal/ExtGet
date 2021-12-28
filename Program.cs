d599d0f1092c228895c1287cf69cd5577ba2bec3
danthespal <55256796+danthespal@users.noreply.github.com>
2021-12-28 16:28:37 +0200

    0.3 BETA
    added total size for specific extension and make it more human readable

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("\nExtGet v0.2 BETA\n");
+                Console.WriteLine("\nExtGet v0.3 BETA\n");
                 Console.WriteLine("you should use the program with a path argument.\n");
                 Console.WriteLine("eg: ExtGet.exe <path>\n");
             }
@@ -27,9 +27,15 @@
             }
         }
 
+        public class FileStatisticInfo
+        {
+            public int Count { get; set; }
+            public long TotalSize { get; set; }
+        }
+
         public static void ProcessFiles(string targetDirectory)
         {
-            Dictionary<string, int> extensions = new();
+            Dictionary<string, FileStatisticInfo> extensions = new();
 
             // count all extensions in directory and sub directory
             void CalcFilesCount(string targetDirectory)
@@ -40,11 +46,16 @@
                 {
                     if (extensions.ContainsKey(ex.Key))
                     {
-                        extensions[ex.Key] += ex.Count();
+                        extensions[ex.Key].Count += ex.Count();
+                        extensions[ex.Key].TotalSize += ex.Sum(filePath => new FileInfo(filePath).Length);
                     }
                     else
                     {
-                        extensions[ex.Key] = ex.Count();
+                        extensions[ex.Key] = new FileStatisticInfo
+                        {
+                            Count = ex.Count(),
+                            TotalSize = ex.Sum(filePath => new FileInfo(filePath).Length)
+                        };
                     }
                 }
 
@@ -56,25 +67,43 @@
 
             CalcFilesCount(targetDirectory);
 
+            // transfer bytes to more human readable
+            static string BytesToString(long byteCount)
+            {
+                string[] suf = { " B", " KB", " MB", " GB", " TB", " PB", " EB" };
+
+                if (byteCount == 0)
+                {
+                    return "0" + suf[0];
+                }
+
+                long bytes = Math.Abs(byteCount);
+                int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+                double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+
+                return (Math.Sign(byteCount) * num).ToString() + suf[place];
+            }
+
+
             // count all the files from directory and sub directory
             int fCount = Directory.GetFiles(targetDirectory, "*", SearchOption.AllDirectories).Length;
 
             // print out the results
-            Console.WriteLine("\nExtGet v0.2 BETA\n");
-            foreach (KeyValuePair<string, int> items in extensions)
+            Console.WriteLine("\nExtGet v0.3 BETA\n");
+            foreach (KeyValuePair<string, FileStatisticInfo> items in extensions)
             {
-                double percentage = (double)Math.Round((double)(items.Value * 100) / fCount, 2);
+                double percentage = (double)Math.Round((double)(items.Value.Count * 100) / fCount, 2);
 
                 if (items.Key == "")
                 {
                     Console.WriteLine($"----------.unknown - {percentage}%");
-                    Console.WriteLine($".unknown : {items.Value} files");
+                    Console.WriteLine($".unknown : {items.Value} files, {BytesToString(items.Value.TotalSize)}");
                     Console.WriteLine("------------------------------\n");
                 }
                 else
                 {
                     Console.WriteLine($"----------{items.Key} - {percentage}%");
-                    Console.WriteLine($"{items.Key} : {items.Value} files");
+                    Console.WriteLine($"{items.Key} : {items.Value.Count} files, {BytesToString(items.Value.TotalSize)}");
                     Console.WriteLine("------------------------------\n");
                 }
             }
